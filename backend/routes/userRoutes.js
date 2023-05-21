fe00f40706e5913df1eab1941640710cd0cb5203
thijsBoet <m.boet@chello.nl>
2023-05-21 15:08:04 +0200

    implemented routes and auth middleware

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const router = express.Router();
+import { protect } from '../middleware/authMiddleware.js';
+
+import {
+	authUser,
+	registerUser,
+	logoutUser,
+	getUserProfile,
+	updateUserProfile,
+} from '../controllers/userControllers.js';
+
+router.post('/', registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,30 @@
 import express from 'express';
-const port = 5000;
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => { 
-    res.send('Server is ready');
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+	res.send('Server is ready');
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+	console.log(`Server started at http://localhost:${port}`);
 });
