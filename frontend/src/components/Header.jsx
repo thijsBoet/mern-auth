ecacf1e9d4b3fb67dda6551732e4f3d5d56f1f47
thijsBoet <m.boet@chello.nl>
2023-05-22 13:56:01 +0200

    build frontend login

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,50 @@
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Header = () => {
+	const { userInfo } = useSelector((state) => state.auth);
+
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<LinkContainer to='/'>
+						<Navbar.Brand>MERN Auth</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							{userInfo ? (
+								<>
+									<NavDropdown title={userInfo.name} id='username'>
+										<LinkContainer to='/profile'>
+											<NavDropdown.Item>Profile</NavDropdown.Item>
+										</LinkContainer>
+										<NavDropdown.Item>Logout</NavDropdown.Item>
+									</NavDropdown>
+								</>
+							) : (
+								<>
+									<LinkContainer to='/login'>
+										<Nav.Link>
+											<FaSignInAlt /> Sign In
+										</Nav.Link>
+									</LinkContainer>
+									<LinkContainer to='/register'>
+										<Nav.Link>
+											<FaSignOutAlt /> Sign Up
+										</Nav.Link>
+									</LinkContainer>
+								</>
+							)}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import App from './App.jsx';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index={true} path='/' element={<HomeScreen />} />
+			<Route path='/login' element={<LoginScreen />} />
+			<Route path='/register' element={<RegisterScreen />} />
+		</Route>,
+	),
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+	</Provider>,
+);
