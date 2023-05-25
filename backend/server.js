eee7b0766af82aac26d865c2fb876764bffe11c6
thijsBoet <m.boet@chello.nl>
2023-05-25 12:08:33 +0200

    ready to deploy via Render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 import userRoutes from './routes/userRoutes.js';
@@ -16,11 +17,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => {
-	res.send('Server is ready');
-});
+if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.resolve();
+	app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.send('Server is ready');
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
